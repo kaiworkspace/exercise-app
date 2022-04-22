e74ab1fa7e1541086e4d5d81d43720fbda9a11d6
Kai Heng <johnloh1998@gmail.com>
2022-04-22 22:02:10 +0800

    added link

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 import StartExercise from "../pages/StartExercise"
 import AddExercise from "../pages/AddExercise"
+import Home from "../pages/Home"
 
 export default function App(){
     return (
         <div>
             <Routes>
-                <Route path="/" element={<StartExercise />}></Route>
+                <Route path="/" element={<Home />}></Route>
+                <Route path="/start-exercise" element={<StartExercise />}></Route>
                 <Route path="/add-exercise" element={<AddExercise />}></Route>
             </Routes>
         </div>
